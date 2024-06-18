af0f8e5680081a37c1d133b64173faecf4aa5f16
kaktos <sticknumber123@gmail.com>
2024-06-18 22:30:46 +0330

    Add StringsProblems.Hard.RegularExpressionMatching and its test

--- src/StringProblems/StringsProblems/Hard/RegularExpressionMatching.cs
+++ src/StringProblems/StringsProblems/Hard/RegularExpressionMatching.cs
@@ -1,3 +1,5 @@
+using Microsoft.Win32.SafeHandles;
+
 namespace StringsProblems.Hard;
 
 public class RegularExpressionMatching
@@ -10,14 +12,40 @@ public class RegularExpressionMatching
     /// <returns></returns>
     public bool IsMatch(string s, string p)
     {
-        var index = 0;
+        var position = 0;
         for (var i = 0; i < p.Length; i++)
         {
-            if(p[i] is '*' or '.') break;
-            if(p[i] == '*' || p[i] == '.') break;
-            
-            index++;
+            if (p[i] is '*') continue;
+
+            if (position == s.Length)
+            {
+                if (p[i] != '.' || p[i] != '*')
+                {
+                    // TODO: when S is finished and last P was *, from end to start of P and S, start checking again.
+                    if (i + 1 < p.Length && p[i + 1] == '*') continue;
+                    
+                    if (i - 1 >= 0 && p[i - 1] == '*')
+                    {
+                        if (i - 3 >= 0 && p[i - 3] == s[^1]) return false;
+                        return s.EndsWith(p[i..]);
+                    }
+
+                    return false;
+                }
+            }
+
+            var isNextAstrix = i + 1 < p.Length && p[i + 1] is '*';
+            while (position < s.Length)
+            {
+                if (p[i] != '.' && p[i] != s[position]) break;
+
+                position++;
+                if (!isNextAstrix) break;
+            }
         }
+
+        if (position == s.Length) return true;
+        if (position - 1 != s.Length) return false;
 
         return true;
     }
--- tests/StringsProblems.UnitTests/Hard/RegularExpressionMatchingTests.cs
+++ tests/StringsProblems.UnitTests/Hard/RegularExpressionMatchingTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using StringsProblems.Hard;
 
 namespace StringsProblems.UnitTests.Hard;
@@ -12,6 +13,16 @@ public class RegularExpressionMatchingTests
         yield return new object[] {"aa", "a", false};
         yield return new object[] {"aa", "a*", true};
         yield return new object[] {"aa", ".*", true};
+        yield return new object[] {"ab", ".*", true};
+        yield return new object[] {"aab", "c*a*b", true};
+        yield return new object[] {"mississippi", "mis*is*ip*.", true};
+        yield return new object[] {"mississippi", "mis*is*p*.", false};
+        yield return new object[] {"ab", ".*c", false};
+        yield return new object[] {"aaa", "a*a", true};
+        yield return new object[] {"aaa", "aaaa", false};
+        yield return new object[] {"aaa", "ab*a*c*a", true};
+        yield return new object[] {"a", "ab*a", false};
+        yield return new object[] {"ab", ".*..", true};
     }
 
     [Theory]
