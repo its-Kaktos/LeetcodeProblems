23a2d134e09b35940772fcd874841e514dfb81f6
Arman Hashempour <sticknumber123@gmail.com>
2024-06-18 12:13:12 +0330

    Add unit test project for StringProblems

--- src/StringProblems/StringsProblems/Program.cs
+++ src/StringProblems/StringsProblems/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using StringsProblems.Medium;
+
 Console.WriteLine("Hello, World!");
+
+
+// var result = new StringToIntegerAtoi().MyAtoi("42");
+// var result = new StringToIntegerAtoi().MyAtoi("words and 987");
+// var result = new StringToIntegerAtoi().MyAtoi("1337c0d");
+// var result = new StringToIntegerAtoi().MyAtoi("-91283472332");
+var result = new StringToIntegerAtoi().MyAtoi("+1");
+// var result = new StringToIntegerAtoi().MyAtoi("+-12");
+// var result = new StringToIntegerAtoi().MyAtoi("-5-");
+
+
+Console.WriteLine(result);
---
+++ tests/StringsProblems.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace StringsProblems.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
